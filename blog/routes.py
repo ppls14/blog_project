b2aac1c017023b123b43fb27bf13828ff3675b29
Remik Tkaczyk <r.t.kodilla@gmail.com>
2023-03-08 19:15:00 +0100

    Add login_required, login and logout functions in routes.py, add login_form.html, add div in base.html.

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template, request, flash
+from flask import render_template, request, session, flash, redirect, url_for
 from blog import app, forms
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
 
 @app.route("/")
 def index():
@@ -10,6 +19,7 @@ def index():
    return render_template("homepage.html", all_posts=all_posts)
 
 @app.route("/edit-post/<post_id>", methods = ["GET", "POST"]) 
+@login_required
 def edit_entry(post_id):
     post = Entry.query.filter_by(id=post_id).first()
     form = forms.EntryForm(obj=post)
@@ -34,3 +44,26 @@ def edit_entry(post_id):
         else:
             errors = form.errors
     return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
